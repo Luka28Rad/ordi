be8e473d45473b268c14d6227d1a0e6befba6c1b
Luka28Rad <luka28rad@gmail.com>
2023-12-05 21:19:22 +0100

    prvi commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float m_JumpForce = 600f;							// Amount of force added when the player jumps.
+    [SerializeField] private float m_DashForce = 600f;	
+    [SerializeField] private float moveSpeed = 10f;
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
+	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
+	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
+
+	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool m_Grounded;            // Whether or not the player is grounded.
+    private bool canDash;
+	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+	private Rigidbody2D m_Rigidbody2D;
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	private Vector3 m_Velocity = Vector3.zero;
+    private float input;
+    private bool toJump = false;
+    private bool toDash = false;
+    private float timeSinceLastDash = 0f;
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+    private void Update() {
+        input = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            toJump = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            toDash = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.P)){
+            transform.position = new Vector3(-7f, -3.5f, 0);
+        }
+    }
+
+	private void FixedUpdate()
+	{
+		bool wasGrounded = m_Grounded;
+		m_Grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				m_Grounded = true;
+                canDash = true;
+			}
+		}
+        Move(input * Time.fixedDeltaTime * moveSpeed, toJump, toDash);
+        toJump = false;
+        toDash = false;
+	}
+
+
+	public void Move(float move, bool jump, bool dash)
+	{
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+        // And then smoothing it out and applying it to the character
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+        // If the input is moving the player right and the player is facing left...
+        if (move > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (move < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+		// If the player should jump...
+		if (m_Grounded && jump)
+		{
+			// Add a vertical force to the player.
+			m_Grounded = false;
+			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+		}
+
+        if(canDash && dash && Time.time - timeSinceLastDash > 0.3f){
+            canDash = false;
+            timeSinceLastDash = Time.time;
+
+            if(m_FacingRight){
+                m_Rigidbody2D.AddForce(new Vector2(m_DashForce, 0f));
+            }
+            else if(!m_FacingRight){
+                m_Rigidbody2D.AddForce(new Vector2(-1f * m_DashForce, 0f));
+            }
+			
+        }
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
