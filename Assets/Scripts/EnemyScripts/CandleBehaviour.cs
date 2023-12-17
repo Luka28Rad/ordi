1fbb1dc60557a20ffd02f4f5496bbefd201abc8c
Zir2904 <73187834+Zir2904@users.noreply.github.com>
2023-12-17 01:55:04 +0100

    Sound effects
    Sound effect paljenja velikog plamena svjetlane i odskok od gljivana (2 nasumicne verzija zvuka) - laka promjena ako se nadu bolji zvukovi

--- Assets/Scripts/EnemyScripts/CandleBehaviour.cs
+++ Assets/Scripts/EnemyScripts/CandleBehaviour.cs
@@ -7,10 +7,13 @@ public class CandleBehaviour : MonoBehaviour
     [SerializeField] GameObject bigFire;
     [SerializeField] GameObject smallFire;
     [SerializeField] Animation fireAnimation;
+
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         bigFire.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
         smallFire.SetActive(true);
         StartCoroutine(Fire());
     }
@@ -22,6 +25,7 @@ public class CandleBehaviour : MonoBehaviour
         fireAnimation.Play();
         yield return new WaitForSeconds(1.5f);  //small fire duration
         bigFire.SetActive(true);
+        audioSource.Play();
         smallFire.SetActive(false);
         fireAnimation.Play();
         yield return new WaitForSeconds(4f);    //big fire duration
