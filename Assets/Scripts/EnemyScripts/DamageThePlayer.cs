42b06e0ebb14cebc3d70e280c9f27bba6a39716f
Luka28Rad <luka28rad@gmail.com>
2024-01-09 10:44:19 +0100

    fixing bugs goes brr

--- Assets/Scripts/EnemyScripts/DamageThePlayer.cs
+++ Assets/Scripts/EnemyScripts/DamageThePlayer.cs
@@ -6,21 +6,10 @@ public class DamageThePlayer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Vector2 velTemp;
         if (collision.gameObject.layer == 3)    //CHECK IF PLAYER
         { 
             collision.GetComponent<HealthManager>().TakeDamage();
-            collision.GetComponent<PlayerController>().NoMove();
-            velTemp = collision.GetComponent<Rigidbody2D>().velocity;
-            collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            if (Mathf.Abs(velTemp.normalized.x) < 0.5f)
-            {
-                collision.GetComponent<Rigidbody2D>().AddForce(new Vector2(-collision.transform.localScale.x * 0.5f, 0.8f) * 8f, ForceMode2D.Impulse);
-            }
-            else
-            {
-                collision.GetComponent<Rigidbody2D>().AddForce(new Vector2(-velTemp.normalized.x, 0.8f) * 8f, ForceMode2D.Impulse);
-            }    
+            //Napravi da player bude imun na damge neko kratko vrijeme dodaj coroutine u damage the player
             collision.GetComponent<SpriteRenderer>().color = Color.red;
             StartCoroutine(BackToNormalColor(collision.GetComponent<SpriteRenderer>(), Color.white, 0.25f, collision));
         }
