7aa548b37f0dab1802f06c183d67ea5705336366
Zir2904 <73187834+Zir2904@users.noreply.github.com>
2024-12-19 21:27:46 +0100

    DashIndicator

--- Assets/Scripts/Multiplayer/DashIndicatorMP.cs
+++ Assets/Scripts/Multiplayer/DashIndicatorMP.cs
@@ -2,25 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Mirror;
 
-public class DashIndicatorFollow : MonoBehaviour
+public class DashIndicatorMP : NetworkBehaviour
 {
     GameObject player;
     float speed = 11f;
     // Start is called before the first frame update
     void Start()
     {
-        if(SceneManager.GetActiveScene().name == "MultiplayerLevel"){
-            return;
-        } else {
-            player = GameObject.FindGameObjectWithTag("Player");
-        }
+        player = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(SceneManager.GetActiveScene().name == "MultiplayerLevel") return;
         if (player != null && Vector2.Distance(player.transform.position, transform.position) > 30)
         {
             transform.position = player.transform.position + new Vector3(0.7f, 0.4f, 0);
@@ -34,6 +30,5 @@ public class DashIndicatorFollow : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position + new Vector3(-0.7f, 0.4f, 0), speed * Time.deltaTime);
         }
-        
     }
 }
