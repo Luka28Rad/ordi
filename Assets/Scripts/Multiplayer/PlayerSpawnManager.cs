037c5584ba41ea20b6f29222ea2e7171dbd8ee69
Zir2904 <73187834+Zir2904@users.noreply.github.com>
2025-01-03 13:32:12 +0100

    Male promjene na deathlineu i countdownu
    -Boja linije
    -novi red za brojke countdowna

--- Assets/Scripts/Multiplayer/PlayerSpawnManager.cs
+++ Assets/Scripts/Multiplayer/PlayerSpawnManager.cs
@@ -23,19 +23,19 @@ private IEnumerator CountdownToStart()
         while (countdown > 0)
         {
             // Update the text to show the countdown
-            countdownText.text = $"Game starts in {countdown}...";
+            countdownText.text = $"Game starts in /n{countdown}";
             yield return new WaitForSeconds(1); // Wait for 1 second
             countdown--;
         }
 
         // Clear the text and invoke the event
+        OnAllPlayersReady?.Invoke();
         countdownText.text = "Go!";
         yield return new WaitForSeconds(1f); // Optional delay before clearing
         countdownText.text = ""; // Clear text after the countdown
    
    
    // yield return new WaitForSeconds(5); // 5-second countdown
-    OnAllPlayersReady?.Invoke();
 }
 
 }
