04f4a91052f3c04b79d59a8f08ec28dbcf370136
Zir2904 <73187834+Zir2904@users.noreply.github.com>
2025-01-04 13:54:47 +0100

    Valjda nepotreban kod zakomentiran zasada, fiksacija pozicije
    movement roditeljskog objekta mp igraca zakomentiran
    fiksirane spawn pozicije mp igraca

--- Assets/Scripts/Multiplayer/PlayerMovementMultiplayerController.cs
+++ Assets/Scripts/Multiplayer/PlayerMovementMultiplayerController.cs
@@ -19,7 +19,7 @@ public class PlayerMovementMultiplayerController : NetworkBehaviour
     void Update(){
         if(SceneManager.GetActiveScene().name == "MultiplayerLevel"){
             if(playerModel.activeSelf == false){SetPosition(); playerModel.SetActive(true); PlayerCosmeticsSetup();}
-            if(isOwned) Movement();
+           // if(isOwned) Movement();
         }  else if(SceneManager.GetActiveScene().name == "LobbyScene"){
             var spriteRenderer = playerModel.GetComponent<SpriteRenderer>();
             var playerController = playerModel.GetComponent<PlayerControllerMP>();
@@ -30,18 +30,21 @@ public class PlayerMovementMultiplayerController : NetworkBehaviour
     }
 
     public void SetPosition(){
-        transform.position = new Vector3(Random.Range(-5,5),0.5f,0); 
+        transform.position = new Vector3(0,0,0); 
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        transform.GetChild(0).transform.GetChild(1).transform.position = new Vector3(Random.Range(-4.5f,4.5f),0.5f,0);
+        transform.GetChild(0).transform.GetChild(1).transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
 
-        void Movement()
+/*        void Movement()
         {
             float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
+            ////float v = Input.GetAxis("Vertical");
 
-            Vector3 dir = new Vector3(h, 0, v);
-            transform.position += dir.normalized * (Time.deltaTime * speed);
+            //Vector3 dir = new Vector3(h, 0, v);
+            //transform.position += dir.normalized * (Time.deltaTime * speed);
             //transofrm.position += dir * speed;
-        }
+        }*/
     
     public void PlayerCosmeticsSetup(){
         PlayerMesh.sprite = PlayerColors[GetComponent<PlayerObjectController>().PlayerColor];
